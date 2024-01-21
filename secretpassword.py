aa9110172acc030d3b0e135c2e667bc5bdcdf8ee
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2024-01-20 23:50:48 -0500

    Create secretpassword.py

---
+++ secretpassword.py
@@ -0,0 +1,11 @@
+#you will have to create the SecretPasswordFile1.txt
+passwordFile = open('SecretPasswordFile1.txt')
+secretPassword = passwordFile.read()
+print('Enter your password.')
+typedPassword = input()
+if typedPassword == secretPassword:
+    print('Access granted')
+    if typedPassword == '12345':
+        print('That password is one that an idiot puts on their luggage.')
+else:
+    print('Access denied')
